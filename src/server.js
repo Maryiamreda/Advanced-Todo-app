cc3868444820ec482c8c49807b3b51f3a8ed8f1e
Maryiamreda <maryiamreda@gmail.com>
2025-02-11 01:05:37 +0200

    MongoDB

--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
-const express = require('express');
+import express from 'express'
+// const express = require('express');
+// const mongoose = require('mongoose')
+import mongoose from 'mongoose'
+
 const app = express();
+const dbURL = 'mongodb+srv://useraccess:7878788788@cluster0.r9zdppo.mongodb.net/todolist?retryWrites=true&w=majority';
+mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then((result) => console.log('db connected')).catch((err) => console.log(err))
 //listen for requests
 app.listen(3000);
