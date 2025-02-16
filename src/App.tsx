a5b6e0a72a4715af46ac9ef168ca4666f66cf948
Maryiamreda <maryiamreda@gmail.com>
2025-02-16 23:58:51 +0200

    centering the app

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Todos from './components/Todos'
 function App() {
 
   return (
-    <div className='flex flex-col gap-5 w-[500px]'>
+    <div className='flex flex-col gap-5 w-[400px]'>
       <div>
         <h1 className='text-white'>T O D O</h1>
       </div>
